60e04f62e70fa8cbe81cd9e32a93c66344fb3dd5
Karim Abou Zeid <7303830+kabouzeid@users.noreply.github.com>
2022-06-21 11:45:50 +0200

    add slurm scripts

---
+++ scripts/pretrain.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=48G
+#SBATCH --gres=gpu:2
+#SBATCH --job-name=pmae-pre
+#SBATCH --time=1-00:00
+#SBATCH --partition=trtx-lo
+#SBATCH --begin=now
+#SBATCH --signal=TERM@120
+#SBATCH --mail-user=karim.abou.zeid@rwth-aachen.de
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=slurm_logs/%j_%n_%x.txt
+# format like node+jobname+user+jobid
+
+# check out examples https://github.com/accre/SLURM
+# or this one https://github.com/statgen/SLURM-examples
+# https://github.com/cdt-data-science/cluster-scripts
+
+python main.py --config cfgs/pretrain.yaml --exp_name "$1" --resume
---
+++ scripts/run.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=48G
+#SBATCH --gres=gpu:2
+#SBATCH --job-name=pmae-run
+#SBATCH --time=2-00:00
+#SBATCH --partition=trtx-lo
+#SBATCH --begin=now
+#SBATCH --signal=TERM@120
+#SBATCH --mail-user=karim.abou.zeid@rwth-aachen.de
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=slurm_logs/%j_%n_%x.txt
+# format like node+jobname+user+jobid
+
+# check out examples https://github.com/accre/SLURM
+# or this one https://github.com/statgen/SLURM-examples
+# https://github.com/cdt-data-science/cluster-scripts
+
+python main.py --config cfgs/pretrain.yaml --exp_name "$1" --resume
+python main.py --config cfgs/finetune_modelnet.yaml --finetune_model --exp_name "$1" --ckpts experiments/pretrain/cfgs/"$1"/ckpt-last.pth --resume
+python main.py --test --config cfgs/finetune_modelnet.yaml --exp_name "$1" --ckpts experiments/finetune_modelnet/cfgs/"$1"/ckpt-best.pth
